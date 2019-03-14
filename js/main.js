3f60084cc07a3aefeb788d98d6c35efb72de2f95
Marian <marian.forwk@gmail.com>
2019-03-14 20:59:27 +0100

    add accordion func

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-jQuery(document).ready(function() {
+// Particle JS
+jQuery(document).ready(function () {
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('particles-js', 'assets/particles/particlesjs-config.json', function() {
+  particlesJS.load('particles-js', 'assets/particles/particlesjs-config.json', function () {
     console.log('callback - particles.js config loaded');
   });
+
+  // Accordion
+  let acc = document.getElementsByClassName("acc");
+  let i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("acc-active");
+      let panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
 
 });
