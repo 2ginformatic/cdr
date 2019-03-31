7bd9aa51776cabcfe3e625b4fd162c28c0e5f4de
Marian <marian.forwk@gmail.com>
2019-03-31 19:32:38 +0200

    Add english page

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function () {
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('particles-js', 'assets/particles/particlesjs-config.json', function () {
+  particlesJS.load('particles-js', '/assets/particles/particlesjs-config.json', function () {
     console.log('callback - particles.js config loaded');
   });
 
